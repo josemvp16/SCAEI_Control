9a5fd97caad26baf624e287e48fc12ce681c7085
J.Israel <J.Israel@192.168.1.61>
2015-02-16 21:12:39 -0600

    new commit

---
+++ src/Serial/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Serial;
+
+import java.awt.Dimension;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author J.Israel
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        MonitorAcceso monitorAcceso = new MonitorAcceso();
+        monitorAcceso.setTitle("SCAEI - ITESA ver.1.0");
+        monitorAcceso.setSize(new Dimension(416,334));
+        monitorAcceso.setLocationRelativeTo(null);
+        monitorAcceso.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        monitorAcceso.setVisible(true);
+    }//END main()
+    
+}//END CLASS
